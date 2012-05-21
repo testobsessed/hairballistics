91a4fc2d69ed68cfc63df5f10e39aa03ac263486
Alex Bepple <alex@bepple.de>
2012-05-22 00:26:55 +0200

    Trajectory moves linearly horizontally

--- spec/javascripts/GameSpec.js
+++ spec/javascripts/GameSpec.js
@@ -1,9 +1,43 @@
 var Hairballistics = function() {
-    return { kittens: ['bla'] };
+    return {
+        kittens: ['bla']
+    };
 };
+
+var Trajectory = function(pos) {
+    return {
+        atTime: function(tick) {
+            return Mover.moveX(pos, tick);
+        }
+    };
+};
+
+var Mover = {
+    moveX: function(position, offset) {
+        return Position(position.x + offset, position.y);
+    }
+}
+var Position = function(x, y) {
+    return {
+        x: x,
+        y: y,
+    };
+};
+
 describe('Hairballistics', function() {
     it("has a kitten in the game", function() {
         game = Hairballistics();
         expect(game.kittens.length).toEqual(1);
+    });
+
+    describe('A hairball', function() {
+        it('is at the original position at time = 0', function() {
+            var x = 5;
+            var y = 5;
+            expect(Trajectory(Position(x, y)).atTime(0)).toEqual(Position(5,5));
+        });
+        it('moves in linear fashion', function() {
+            expect(Trajectory(Position(0, 0)).atTime(1)).toEqual(Position(1,0));
+        });
     });
 });
