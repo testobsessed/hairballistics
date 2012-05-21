ae56dff24a32e03027040185aaae8f7f8a31adf6
Alex Bepple <alex@bepple.de>
2012-05-22 00:29:53 +0200

    Move implementation code to implementation file.

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -6,3 +6,29 @@ window.onload = function () {
         context.fillRect(30, 30, 50, 50);
     }
 }
+
+var Hairballistics = function() {
+    return {
+        kittens: ['bla']
+    };
+};
+
+var Trajectory = function(pos) {
+    return {
+        atTime: function(tick) {
+            return Mover.moveX(pos, tick);
+        }
+    };
+};
+
+var Mover = {
+    moveX: function(position, offset) {
+        return Position(position.x + offset, position.y);
+    }
+}
+var Position = function(x, y) {
+    return {
+        x: x,
+        y: y,
+    };
+};
--- spec/javascripts/GameSpec.js
+++ spec/javascripts/GameSpec.js
@@ -1,29 +1,3 @@
-var Hairballistics = function() {
-    return {
-        kittens: ['bla']
-    };
-};
-
-var Trajectory = function(pos) {
-    return {
-        atTime: function(tick) {
-            return Mover.moveX(pos, tick);
-        }
-    };
-};
-
-var Mover = {
-    moveX: function(position, offset) {
-        return Position(position.x + offset, position.y);
-    }
-}
-var Position = function(x, y) {
-    return {
-        x: x,
-        y: y,
-    };
-};
-
 describe('Hairballistics', function() {
     it("has a kitten in the game", function() {
         game = Hairballistics();
@@ -36,6 +10,7 @@ describe('Hairballistics', function() {
             var y = 5;
             expect(Trajectory(Position(x, y)).atTime(0)).toEqual(Position(5,5));
         });
+
         it('moves in linear fashion', function() {
             expect(Trajectory(Position(0, 0)).atTime(1)).toEqual(Position(1,0));
         });
