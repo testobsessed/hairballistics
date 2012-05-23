27657ee0b448dfb690e1fb4c08141a28f44350c9
kbaribeau <kevin.baribeau@gmail.com>
2012-05-22 17:15:34 -0700

    Add vector operations for magnitude.
    Rickard / Kevin

--- public/javascripts/Vector.js
+++ public/javascripts/Vector.js
@@ -6,8 +6,22 @@ var Point = function(x, y) {
 };
 
 var Vector = {
-    add : function(v1, v2) {
+    add: function(v1, v2) {
         return Point(v1.x + v2.x, v1.y + v2.y);
+    },
+
+    magnitude: function(v1) {
+        return Math.sqrt(Math.pow(v1.x, 2) + Math.pow(v1.y, 2));
+    },
+
+    setMagnitude: function(v, mag) {
+        var unitVector = Vector.unitVector(v);
+        return Point(unitVector.x * mag, unitVector.y * mag);
+    },
+
+    unitVector: function(v) {
+        var mag = Vector.magnitude(v);
+        return Point(v.x / mag, v.y / mag);
     },
 }
 
---
+++ spec/javascripts/VectorSpec.js
@@ -0,0 +1,35 @@
+describe('Vector', function() {
+    describe('#add', function() {
+        it("adds two simple vectors", function() {
+            var v1 = Point(21, 21);
+            var v2 = Point(21, 21);
+
+            var result = Vector.add(v1, v2);
+
+            expect(result).toEqual(Point(42, 42));
+        });
+    });
+
+    describe('#magnitude', function() {
+        it("gets the magnitude for a zero vector", function() {
+            expect(Vector.magnitude(Point(0,0))).toEqual(0);
+        });
+
+        it("gets the magnitude for nonzero vectors", function() {
+            expect(Vector.magnitude(Point(0,1))).toEqual(1);
+            expect(Vector.magnitude(Point(1,0))).toEqual(1);
+            expect(Vector.magnitude(Point(5,0))).toEqual(5);
+            expect(Vector.magnitude(Point(0,5))).toEqual(5);
+            expect(Vector.magnitude(Point(5,5))).toEqual(Math.sqrt(50));
+        });
+    });
+
+    describe("#setMagnitude", function() {
+        it("sets magnitude", function() {
+            expect(Vector.magnitude(Vector.setMagnitude(Point(0, 1), 10))).toEqual(10)
+            expect(Vector.magnitude(Vector.setMagnitude(Point(1, 0), 10))).toEqual(10)
+            expect(Vector.magnitude(Vector.setMagnitude(Point(5, 5), 20))).toEqual(20)
+            expect(Vector.magnitude(Vector.setMagnitude(Point(2, 7), 42))).toEqual(42)
+        });
+    });
+});
