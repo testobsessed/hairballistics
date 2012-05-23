1132cb4777cce4b06ad29dddc77f662a163a01ce
Alex Bepple <alex@bepple.de>
2012-05-24 00:00:18 +0200

    Release Hairballistics

---
+++ javascripts/Hairballistics.js
@@ -0,0 +1,66 @@
+var Hairballistics = function() {
+    var margin = 25;
+    var kittenWidth = 59;
+    var right_wall = WIDTH - margin;
+    var left_wall = margin;
+    var ceiling = HEIGHT - margin;
+    var floor = margin;
+
+    var hairball = null;
+    var kitten1 = Kitten(left_wall, 70, "yellow");
+    var kitten2 = Kitten(right_wall-kittenWidth, 70, "gray");
+
+    var spacePressed = false;
+
+    var launchHairball = function(vector) {
+        hairball = Hairball(currentKitten().mouthPosition(), vector);
+        return hairball;
+    };
+
+    var currentKitten = function() {
+        return kitten1;
+    };
+
+    return {
+        tick: function() {
+            if(hairball && !hairball.splatted()) {
+                hairball = hairball.tick();
+                if(hairball.position.y <= (floor + margin) || hairball.position.x >= (right_wall - margin)) {
+                    hairball.splat();
+                }
+            }
+
+            if(spacePressed) {
+                currentKitten().incrementPower();
+            }
+        },
+        withHairball: function(fn) {
+            if (hairball) {
+                fn(hairball);
+            }
+        },
+        withKittens: function(fn) {
+            fn(kitten1);
+            fn(kitten2);
+        },
+        keyDownHandler: function(event) {
+            if (event.keyCode == 32) {
+                spacePressed = true;
+            }
+        },
+        keyUpHandler: function(event) {
+            if (event.keyCode == 32) {
+                spacePressed = false;
+                launchHairball(currentKitten().targettingLine());
+                currentKitten().resetPower();
+            }
+        },
+        launchHairball: launchHairball,
+        currentPower: function() {
+            return currentKitten().targettingLine();
+        },
+        currentKitten: currentKitten,
+        hairballs: function() { return [hairball]; },
+    };
+};
+
