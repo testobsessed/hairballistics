cc347dd446d4af96aeb3eb0459d1b01bdc84e75b
Rickard Lindberg <ricli85@gmail.com>
2012-05-24 01:15:41 +0200

    Move Kitten to separate file.

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -73,40 +73,6 @@ var Renderer = function(context, world) {
     };
 };
 
-var Kitten = function(x, y, color) {
-    var targettingLine = null;
-    var resetPower = function() {
-        targettingLine = Point(1, 1);
-    }
-    resetPower();
-    var fainted = false;
-
-    return {
-        position: Point(x, y),
-        color: color,
-        boundingRectangle: function() {
-            return Rect(x, y+20, 60, 70);
-        },
-        mouthPosition: function() {
-            return Point(x+50, y+25);
-        },
-        targettingLine: function() {
-            return targettingLine;
-        },
-        incrementPower: function() {
-           mag = Vector.magnitude(targettingLine);
-           targettingLine = Vector.setMagnitude(targettingLine, ((mag + .2) % 50) + 1)
-        },
-        resetPower: resetPower,
-        faint: function() {
-            fainted = true;
-        },
-        fainted: function() {
-            return fainted;
-        },
-    };
-};
-
 var Physics = {
     GRAVITY: 1, // in pixels per tick
 
