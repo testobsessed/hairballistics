6200dd3e3ec2dbb62498fc3af869fac265289bb7
Alex Bepple <alex@bepple.de>
2012-05-24 00:22:18 +0200

    Test release script

---
+++ game/javascripts/Collision.js
@@ -0,0 +1,28 @@
+var Rect = function(x, y, w, h) {
+    return {
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        left: x,
+        right: x + w,
+        bottom: y,
+        top: y + h,
+    };
+};
+
+var Collision = {
+    overlap: function(r1, r2) {
+        if (r2.bottom > r1.top) {
+            return false;
+        } else if (r2.top < r1.bottom) {
+            return false;
+        } else if (r2.right < r1.left) {
+            return false;
+        } else if (r2.left > r1.right) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+};
