8f0443fcf54492e35ed6999f9ae1c9e7232f391a
Alex Bepple & Rickard Lindberg <alex.bepple+ab+rl@gmail.com>
2012-05-24 20:58:10 +0200

    Fixed indentation

--- spec/javascripts/KittenSpec.js
+++ spec/javascripts/KittenSpec.js
@@ -1,15 +1,15 @@
 describe('Kitten', function() {
     describe("score", function() {      
-      it("starts kittens with a score of 0", function(){
-        var kitten = Kitten(0, 0, someProperties);
-        expect(kitten.score()).toEqual(0);
-      });
-      
-      it("increments the score", function(){
-        var kitten = Kitten(0, 0, someProperties);
-        kitten.scoredHit();
-        expect(kitten.score()).toEqual(1);        
-      })
+        it("starts kittens with a score of 0", function(){
+            var kitten = Kitten(0, 0, someProperties);
+            expect(kitten.score()).toEqual(0);
+        });
+
+        it("increments the score", function(){
+            var kitten = Kitten(0, 0, someProperties);
+            kitten.scoredHit();
+            expect(kitten.score()).toEqual(1);        
+        })
     });
     
     describe('#resetPower', function() {
