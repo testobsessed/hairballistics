ee158fbfcb9d6cdfb6d70fb470a3f7627c3150ac
Rickard Lindberg <ricli85@gmail.com>
2012-05-24 23:06:44 +0200

    Restructure Hairballistics.js.

--- public/javascripts/Hairballistics.js
+++ public/javascripts/Hairballistics.js
@@ -1,142 +1,180 @@
-var Hairballistics = function() {
-    var margin = 25;
-    var kittenWidth = 59;
-    var right_wall = WIDTH - margin;
-    var left_wall = margin;
-    var ceiling = HEIGHT - margin;
-    var floor = margin;
+var kittenWidth = 59;
+var margin = 25;
+var left_wall = margin;
+var right_wall = WIDTH - margin;
+var floor = margin;
+var positioningFudgeFactor = 30;
 
-    var hairball = null;
+var WorldState = function() {
+    var stateObject = {
+        hairball: null,
+        spacePressed: false,
+        kitten1: Kitten(left_wall + positioningFudgeFactor, 70, {
+            headImage: "orange_head.png",
+            bodyImage: "orange_body.png",
+            headOffset: Point(39, 0),
+            mouthOffset: Point(50, 25),
+            targetingLine: Point(1, 1),
+        }),
+        kitten2: Kitten(right_wall-kittenWidth - positioningFudgeFactor, 70, {
+            headImage: "black_head.png",
+            bodyImage: "black_body.png",
+            headOffset: Point(12, 0),
+            mouthOffset: Point(-10, 25),
+            targetingLine: Point(-1, 1),
+        }),
+        switchPlayer: function() {
+            var tmp = stateObject.kitten1;
+            stateObject.kitten1 = stateObject.kitten2;
+            stateObject.kitten2 = tmp;
+        },
+        currentKitten: function() {
+            return stateObject.kitten1;
+        },
+        opponentKitten: function() {
+            return stateObject.kitten2;
+        },
+        launchHairball: function(vector) {
+            stateObject.hairball = Hairball(stateObject.currentKitten().mouthPosition(), vector);
+            return stateObject.hairball;
+        },
+        withHairball: function(fn) {
+            if (stateObject.hairball) {
+                fn(stateObject.hairball);
+            }
+        },
+        withKittens: function(fn) {
+            fn(stateObject.kitten1);
+            fn(stateObject.kitten2);
+        },
+        setCurrentKitten: function(newKitten) {
+            stateObject.kitten1 = newKitten;
+        },
+        setOpponentKitten: function(newKitten) {
+            stateObject.kitten2 = newKitten;
+        },
+        setHairball: function(newHairball) {
+            stateObject.hairball = newHairball;
+        },
+        currentPower: function() {
+            return stateObject.currentKitten().targetingLine();
+        },
+        hairballs: function() {
+            return [stateObject.hairball];
+        },
+    };
+    return stateObject;
+};
 
-    var positioningFudgeFactor = 70;
-    var kitten1 = Kitten(left_wall + positioningFudgeFactor, 70, {
-        headImage: "orange_head.png",
-        bodyImage: "orange_body.png",
-        headOffset: Point(39, 0),
-        mouthOffset: Point(50, 25),
-        targetingLine: Point(1, 1),
-    });
-
-    var positioningFudgeFactor = 30;
-    var kitten2 = Kitten(right_wall-kittenWidth - positioningFudgeFactor, 70, {
-        headImage: "black_head.png",
-        bodyImage: "black_body.png",
-        headOffset: Point(12, 0),
-        mouthOffset: Point(-10, 25),
-        targetingLine: Point(-1, 1),
-    });
-
+var KeyHandler = function(worldState, triggerEvent) {
     var SPACE = 32;
     var LEFT_ARROW = 37;
     var RIGHT_ARROW = 39;
+
     var keyUpEvents = {};
     var keyDownEvents = {};
 
     keyDownEvents[SPACE] = function() {
-        spacePressed = true;
+        worldState.spacePressed = true;
     };
     keyUpEvents[SPACE] = function() {
-        spacePressed = false;
-        launchHairball(currentKitten().targetingLine());
-        currentKitten().resetPower();
+        worldState.spacePressed = false;
+        worldState.launchHairball(worldState.currentKitten().targetingLine());
+        worldState.currentKitten().resetPower();
     };
 
     keyDownEvents[LEFT_ARROW] = function() {
-        $(world).trigger({ type: 'rotateCounterClockwise'});
-        currentKitten().rotateTargetingLineCounterClockwise();
+        triggerEvent({ type: 'rotateCounterClockwise'});
+        worldState.currentKitten().rotateTargetingLineCounterClockwise();
     };
     keyDownEvents[RIGHT_ARROW] = function() {
-        $(world).trigger({ type: 'rotateClockwise'});
-        currentKitten().rotateTargetingLineClockwise();
+        triggerEvent({ type: 'rotateClockwise'});
+        worldState.currentKitten().rotateTargetingLineClockwise();
     };
 
-    var spacePressed = false;
-
-    var switchPlayer = function() {
-        var tmp = kitten1;
-        kitten1 = kitten2;
-        kitten2 = tmp;
+    return {
+        keyDownHandler: function(event) {
+            if (keyDownEvents[event.keyCode]) {
+                keyDownEvents[event.keyCode]();
+            }
+        },
+        keyUpHandler: function(event) {
+            if (keyUpEvents[event.keyCode]) {
+                keyUpEvents[event.keyCode]();
+            }
+        },
     };
+};
 
-    var launchHairball = function(vector) {
-        hairball = Hairball(currentKitten().mouthPosition(), vector);
-        return hairball;
-    };
-
-    var currentKitten = function() {
-        return kitten1;
-    };
-
-    var opponentKitten = function() {
-        return kitten2;
-    };
-
+var CollisionDetector = function(world) {
     var detectCollision = function(object1, object2) {
         return Collision.overlap(object1.boundingRectangle(), object2.boundingRectangle());
     };
+    return {
+        checkCollisions: function() {
+            if (world.hairball) {
+                if (detectCollision(world.hairball, world.opponentKitten())) {
+                    world.currentKitten().scoredHit();
+                    world.opponentKitten().faint();
+                }
+            }
+
+            if(world.hairball && !world.hairball.splatted()) {
+                if(world.hairball.position.y <= (floor + margin) || world.hairball.position.x >= (right_wall - margin)) {
+                    world.hairball.splat();
+                    world.switchPlayer();
+                }
+            }
+        }
+    };
+};
+
+var Ticker = function(world) {
+    return {
+        tick: function() {
+            if(world.hairball && !world.hairball.splatted()) {
+                world.hairball = world.hairball.tick();
+            }
+            if(world.spacePressed) {
+                world.currentKitten().incrementPower();
+            }
+        },
+    };
+};
+
+var Hairballistics = function() {
+    var worldState = WorldState();
+    var detector = CollisionDetector(worldState);
+    var ticker = Ticker(worldState);
 
     var world = {
         tick: function() {
-            if (hairball) {
-                if (detectCollision(hairball, opponentKitten())) {
-                    currentKitten().scoredHit();
-                    opponentKitten().faint();
-                }
-            }
+            detector.checkCollisions();
+            ticker.tick();
+        },
+        withHairball: worldState.withHairball,
+        withKittens: worldState.withKittens,
+        launchHairball: worldState.launchHairball,
+        currentPower: worldState.currentPower,
+        currentKitten: worldState.currentKitten,
+        hairballs: worldState.hairballs,
+        setHairball: worldState.setHairball,
+        setCurrentKitten: worldState.setCurrentKitten,
+        setOpponentKitten: worldState.setOpponentKitten,
+    };
 
-            if(hairball && !hairball.splatted()) {
-                hairball = hairball.tick();
-                if(hairball.position.y <= (floor + margin) || hairball.position.x >= (right_wall - margin)) {
-                    hairball.splat();
-                    switchPlayer();
-                }
-            }
+    var keyHandler = KeyHandler(worldState, function(e) {
+        $(world).trigger(e);
+    });
+    world.keyDownHandler = keyHandler.keyDownHandler;
+    world.keyUpHandler = keyHandler.keyUpHandler;
 
-            if(spacePressed) {
-                currentKitten().incrementPower();
-            }
-        },
-        withHairball: function(fn) {
-            if (hairball) {
-                fn(hairball);
-            }
-        },
-        withKittens: function(fn) {
-            fn(kitten1);
-            fn(kitten2);
-        },
-        launchHairball: launchHairball,
-        currentPower: function() {
-            return currentKitten().targetingLine();
-        },
-        currentKitten: currentKitten,
-        hairballs: function() { return [hairball]; },
-        setHairball: function(newHairball) {
-            hairball = newHairball;
-        },
-        setCurrentKitten: function(newKitten) {
-            kitten1 = newKitten;
-        },
-        setOpponentKitten: function(newKitten) {
-            kitten2 = newKitten;
-        },
-    };
-    world.keyDownHandler = function(event) {
-        if (keyDownEvents[event.keyCode]) {
-            keyDownEvents[event.keyCode]();
-        }
-    };
-    world.keyUpHandler = function(event) {
-        if (keyUpEvents[event.keyCode]) {
-            keyUpEvents[event.keyCode]();
-        }
-    };
     world.onRotateClockwise = function(handler) {
         $(world).on('rotateClockwise', handler);
     };
     world.onRotateCounterClockwise = function(handler) {
         $(world).on('rotateCounterClockwise', handler);
     };
+
     return world;
 };
-
