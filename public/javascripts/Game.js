6d90760f56d6d603849aa8ba416902e334b95dc0
Rickard Lindberg <ricli85@gmail.com>
2012-05-25 01:47:31 +0200

    Rename.

--- public/javascripts/Game.js
+++ public/javascripts/Game.js
@@ -2,24 +2,25 @@ var WIDTH = 1000;
 var HEIGHT = 500;
 
 $(document).ready(function() {
-    var world = Hairballistics();
-    var ticker = Ticker(world.worldState);
-    var detector = CollisionDetector(world.worldState);
+    var hairballistics = Hairballistics();
+    var world = hairballistics.worldState;
+    var ticker = Ticker(world);
+    var detector = CollisionDetector(world);
 
-    $(document).on('keydown', world.keyDownHandler);
-    $(document).on('keyup', world.keyUpHandler);
+    $(document).on('keydown', hairballistics.keyDownHandler);
+    $(document).on('keyup', hairballistics.keyUpHandler);
     // If we are in test, do not set the game loop/create a renderer
     if(document.getElementById('game')) {
 
-        var renderer = Renderer("game", world.worldState);
+        var renderer = Renderer("game", world);
 
         var redraw = function() {
             detector.checkCollisions();
             ticker.tick();
             renderer.redraw();
         };
-        world.onRotateClockwise(renderer.rotateKittenHeadClockwise);
-        world.onRotateCounterClockwise(renderer.rotateKittenHeadCounterClockwise);
+        hairballistics.onRotateClockwise(renderer.rotateKittenHeadClockwise);
+        hairballistics.onRotateCounterClockwise(renderer.rotateKittenHeadCounterClockwise);
         setInterval(redraw, 24); // ~48 fps
     }
 });
