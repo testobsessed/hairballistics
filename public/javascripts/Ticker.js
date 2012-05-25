dbe8bdfe2d3e0bcaaee9108ce868af9f897b1ddc
Matthew Barcomb + Alex Bepple <alex+mb+ab@bepple.de>
2012-05-25 19:54:36 +0200

    Extracted Ticker to its own file
    Hairballistics.js is now empty.

---
+++ public/javascripts/Ticker.js
@@ -0,0 +1,12 @@
+var Ticker = function(world) {
+    return {
+        tick: function() {
+            if(world.hairball && !world.hairball.splatted()) {
+                world.hairball = world.hairball.tick();
+            }
+            if(world.spacePressed) {
+                world.currentKitten().incrementPower();
+            }
+        },
+    };
+};
